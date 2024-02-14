6a0c7066983a120c5dca79c76f0d55e80a551877
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-02-14 15:39:12 +0100

    fetch locations with sg

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ fetchSiteData("MolndalCentrum").then((siteData) => {
   console.log(`SG: ${siteData.sg}`);
   console.log(`DG: ${siteData.dg}`);
 
-  valuesStartEnd("2023-01-01", "2023-12-31", "MolndalCentrum").then(
+  valuesStartEnd("2023-01-01", "2023-01-02", "MolndalCentrum").then(
     (measurements) => {
       measurements.forEach((measurement) => {
         console.log(`Date: ${measurement.date}`);
@@ -20,20 +20,22 @@ fetchSiteData("MolndalCentrum").then((siteData) => {
   );
 });
 
+measureLocation().then((locations) => {
+  //bygg options lista från denna data
+  console.log(locations);
+});
+
 threeInit();
 
 document
   .querySelector('form[name="data"]')
   .addEventListener("submit", function (event) {
-    // Prevent the form from being submitted
     event.preventDefault();
 
-    // Get the form elements
     const location = document.getElementById("location").value;
     const startDate = document.getElementById("startdate").value;
     const endDate = document.getElementById("enddate").value;
 
-    // Log the form data
     console.log("Location:", location);
     console.log("Start Date:", startDate);
     console.log("End Date:", endDate);
--- modules/fetchApi.js
+++ modules/fetchApi.js
@@ -8,7 +8,10 @@ export function fetchSiteData(location) {
     .then((data) => ({
       sg: data.SG,
       dg: data.DG,
-    }));
+    }))
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 export function valuesStartEnd(startDate, endDate, location) {
@@ -21,9 +24,26 @@ export function valuesStartEnd(startDate, endDate, location) {
         value: entry.Value,
         date: new Date(parseInt(entry.TimeStamp.substr(6))),
       }))
-    );
+    )
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 export function measureLocation() {
-  return x - y;
+  return fetch(
+    "https://data.goteborg.se/RiverService/v1.1/MeasureSites/2e864a63-859d-46b7-9c87-dd966f87595e?format=json"
+  )
+    .then((response) => response.json())
+    .then((data) =>
+      data
+        .filter((entry) => entry.hasOwnProperty("SG"))
+        .map((entry) => ({
+          Code: entry.Code,
+          Description: entry.Description,
+        }))
+    )
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
