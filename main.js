6277dff1a6de2bb927d5511d025b0cc067be9064
OsborneLinus <linusholm1997@gmail.com>
2024-02-14 15:39:13 +0100

    added logic from fetch to form

--- main.js
+++ main.js
@@ -6,35 +6,27 @@ import {
   fetchSiteData,
 } from "./modules/fetchApi.js";
 
-fetchSiteData("MolndalCentrum").then((siteData) => {
-  console.log(`SG: ${siteData.sg}`);
-  console.log(`DG: ${siteData.dg}`);
-
-  valuesStartEnd("2023-01-01", "2023-12-31", "MolndalCentrum").then(
-    (measurements) => {
-      measurements.forEach((measurement) => {
-        console.log(`Date: ${measurement.date}`);
-        console.log(`Value: ${measurement.value}`);
-      });
-    }
-  );
-});
-
-threeInit();
-
 document
   .querySelector('form[name="data"]')
   .addEventListener("submit", function (event) {
     // Prevent the form from being submitted
     event.preventDefault();
-
     // Get the form elements
     const location = document.getElementById("location").value;
     const startDate = document.getElementById("startdate").value;
     const endDate = document.getElementById("enddate").value;
 
-    // Log the form data
-    console.log("Location:", location);
-    console.log("Start Date:", startDate);
-    console.log("End Date:", endDate);
+    fetchSiteData(location).then((siteData) => {
+      console.log(`SG: ${siteData.sg}`);
+      console.log(`DG: ${siteData.dg}`);
+
+      valuesStartEnd(startDate, endDate, location).then((measurements) => {
+        measurements.forEach((measurement) => {
+          console.log(`Date: ${measurement.date}`);
+          console.log(`Value: ${measurement.value}`);
+        });
+      });
+    });
   });
+
+threeInit();
