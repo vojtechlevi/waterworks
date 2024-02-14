88493729965998314b426ee6650b19510a8f8569
OsborneLinus <linusholm1997@gmail.com>
2024-02-14 15:09:22 +0100

    Added form for changing the date and location

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 import { threeInit } from "./modules/threeInit";
 
 threeInit();
+
+document
+  .querySelector('form[name="data"]')
+  .addEventListener("submit", function (event) {
+    // Prevent the form from being submitted
+    event.preventDefault();
+
+    // Get the form elements
+    const location = document.getElementById("location").value;
+    const startDate = document.getElementById("startdate").value;
+    const endDate = document.getElementById("enddate").value;
+
+    // Log the form data
+    console.log("Location:", location);
+    console.log("Start Date:", startDate);
+    console.log("End Date:", endDate);
+  });
