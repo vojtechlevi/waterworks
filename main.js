b20e12936cff442020c5cd5d2fca7a4b3718fd44
OsborneLinus <linusholm1997@gmail.com>
2024-02-20 11:47:29 +0100

    Added watercontainer for readability of waterlevel

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import { threeInit, setMeasurements } from "./modules/threeInit";
-
 import {
   valuesStartEnd,
   measureLocation,
   fetchSiteData,
 } from "./modules/fetchApi.js";
+import { FluidMeter } from "./modules/js-fluid-meter.js";
 
 measureLocation().then((locations) => {
   const dropdown = document.getElementById("location");
@@ -50,3 +50,38 @@ const endDateInput = document.getElementById("enddate");
 startDateInput.addEventListener("change", function () {
   endDateInput.min = this.value;
 });
+
+var fm3 = new FluidMeter();
+fm3.init({
+  targetContainer: document.getElementById("fluid-meter-3"),
+  fillPercentage: 45,
+  options: {
+    fontSize: "30px",
+    drawPercentageSign: true,
+    drawBubbles: false,
+    size: 300,
+    borderWidth: 0,
+    backgroundColor: "#e2e2e2",
+    foregroundColor: "#1F2937",
+    foregroundFluidLayer: {
+      fillStyle: "#16E1FF",
+      angularSpeed: 30,
+      maxAmplitude: 5,
+      frequency: 30,
+      horizontalSpeed: -20,
+    },
+    backgroundFluidLayer: {
+      fillStyle: "#4F8FC6",
+      angularSpeed: 100,
+      maxAmplitude: 3,
+      frequency: 22,
+      horizontalSpeed: 20,
+    },
+  },
+});
+
+window.onload = function () {
+  document.getElementById("submit-percentage-3").onclick = function () {
+    fm3.setPercentage(Number(document.getElementById("percentage-3").value));
+  };
+};
